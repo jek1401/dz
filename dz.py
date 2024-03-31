39592c2da6fb4cb8b9abc283cb9549544e266873
jek1401 <138993215+jek1401@users.noreply.github.com>
2024-03-31 11:57:30 +0300

    dz.py

---
+++ dz.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+import os
+import random
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def mem(ctx):
+    img_name = random.choise(os.listdir('images'))
+    with open(f"images/{img_name}", 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def animal(ctx):
+    img_name = random.choise(os.listdir('images'))
+    with open(f"animals/{img_name}", 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+
+bot.run("YOUR TOKEN")
